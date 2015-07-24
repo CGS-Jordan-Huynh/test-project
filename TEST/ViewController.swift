326e3b2b6091b9607e6d1330f3bf9ce0f09a5f69
Jordan Huynh <2407@cgs.act.edu.au>
2015-07-24 12:51:34 +1000

    Intial commit

---
+++ TEST/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TEST
+//
+//  Created by Jordan Huynh on 24/07/2015.
+//  Copyright (c) 2015 Apress. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        println("Salutations")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
