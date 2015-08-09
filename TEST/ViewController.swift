ee1b2345c42e1350636f3a0637ac6eaae51ece91
Jordan Huynh <2407@cgs.act.edu.au>
2015-08-10 09:14:21 +1000

    Added vegetable func

--- TEST/ViewController.swift
+++ TEST/ViewController.swift
@@ -21,6 +21,9 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    func vegetable () {
+        
+    }
 
 
 }
